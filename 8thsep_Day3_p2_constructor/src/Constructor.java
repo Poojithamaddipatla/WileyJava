3bcc3a0e77cef799648c1796540b51561b6de32d
Poojitha Maddipatla <Poojitha Maddipatla@Pooji>
2022-09-13 10:43:29 +0530

    commit

---
+++ 8thsep_Day3_p2_constructor/src/Constructor.java
@@ -0,0 +1,16 @@
+
+class Constructor {
+	String name;
+	Constructor()
+	{
+		name="Poojitha";
+		System.out.println(name);
+		
+	}
+	public Constructor(String n)
+	{
+		name=n;
+		System.out.println(name);
+	}
+
+}
---
+++ 8thsep_Day3_p2_constructor/src/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		Constructor t1=new Constructor();
+		Constructor t2=new Constructor("Anand");
+		Constructor t3=new Constructor("Udaya");
+		Constructor t4=new Constructor("Shubhendra");
+		Constructor t5=new Constructor("Akanksha");
+		Constructor t6=new Constructor("Akash");
+		
+	}
+
+}
